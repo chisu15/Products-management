676b14bfedd5b86a0d106ae00d41630bc37990a9
chisu15 <trunghieudo1509@gmail.com>
2023-08-29 23:42:25 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT;
 const app = express();
-const port = 3000;
+
 
 app.set("views", "./views")
 app.set("view engine", "pug");
