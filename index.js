5ce070ddf8814ca672587d072f0e7e9a665edf15
chisu15 <trunghieudo1509@gmail.com>
2023-08-30 09:01:23 +0700

    Nhúng Bootstrap 4 - file tĩnh

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.set("views", "./views")
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 //  ROUTER
 const route = require ("./routes/client/index_route");
 route(app)
